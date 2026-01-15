75041935f17c909acb6ba19c705a4f2b222a0d8f
Yiannis <ioannis.roussos@hotmail.com>
2026-01-15 11:37:31 +0200

    moved .map around only the card. removed backticks and $

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -28,8 +28,7 @@ const HotCollections = () => {
     
     
     
-    users.map((user) => (
-      `<section id="section-collections" className="no-bottom">
+    <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -39,33 +38,36 @@ const HotCollections = () => {
             </div>
           </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key=${user.id}>
+        {users.map((user) => (
+
+          <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={user.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src=${user.nftImage} className="lazy img-fluid" alt="" />
+                    <img src={user.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src=${user.AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={user.AuthorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{user.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{user.code}</span>
                 </div>
               </div>
             </div>
-          
+))
+}          
         </div>
       </div>
-    </section>`
-        ))
-  );
+    </section>
+        )
+  
 };
 
 
