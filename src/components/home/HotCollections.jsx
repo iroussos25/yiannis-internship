2c525d670554136f6c4c253fedd2b2f9af092f8d
Yiannis <ioannis.roussos@hotmail.com>
2026-01-17 18:41:55 +0200

    fixed spacing and centering for hot collections

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -5,7 +5,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider from "react-slick";
 import { Box, Skeleton, Stack } from "@mui/material";
-
+import "./HotCollections.css";
 
 
 const HotCollections = () => {
