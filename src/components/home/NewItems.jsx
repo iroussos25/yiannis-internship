6e864c1395798e754945d6c370499eb66ae6c229
Yiannis <ioannis.roussos@hotmail.com>
2026-01-17 20:03:15 +0200

    trying to find the correct structure

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
 
 const NewItems = () => {
@@ -36,70 +34,80 @@ const NewItems = () => {
             </div>
           </div>
 
+{loading ?(
 
-          {users.map((user) => (
-            {new Array(4).fill(0).map((_, index) => (
-                
-                <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                    >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
+  new Array(4).fill(0).map((_, index) => (
+    <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+      <div className="nft__item">
+      <div className="author_list_pp">
+      <div className="lazy skeleton" style={{width: '50px', height: '50px', borderRadius: '50%'}}></div>
+      </div>
+      <div className="de_countdown skeleton" style={{width: '80px', height: '20px'}}></div>
+  ))
+) : (
+    {users.map((user) => (
+      
+      <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={user.authorId}>
+      <div className="nft__item">
+      <div className="author_list_pp">
+      <Link
+      to="/author"
+      data-bs-toggle="tooltip"
+      data-bs-placement="top"
+      title="Creator: Monica Lucas"
+      >
+      <img className="lazy" src={user.authorImage} alt="" />
+      <i className="fa fa-check"></i>
+      </Link>
+      </div>
+      <div className="de_countdown">5h 30m 32s</div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
+      <div className="nft__item_wrap">
+      <div className="nft__item_extra">
+      <div className="nft__item_buttons">
+      <button>Buy Now</button>
+      <div className="nft__item_share">
                         <h4>Share</h4>
                         <a href="" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
+                          </a>
+                          <a href="" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
+                          </a>
+                          <a href="">
                           <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
+                          </a>
+                          </div>
+                          </div>
+                          </div>
+                          
+                          <Link to="/item-details">
+                          <img
+                          src={user.nftImage}
+                          className="lazy nft__item_preview"
                       alt=""
                       />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
+                      </Link>
+                      </div>
+                      <div className="nft__item_info">
+                      <Link to="/item-details">
+                      <h4>{user.title}</h4>
+                      </Link>
                   <div className="nft__item_price">3.08 ETH</div>
                   <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
+                  <i className="fa fa-heart"></i>
                     <span>69</span>
                   </div>
-                </div>
+                  </div>
+                  </div>
+                  </div>
+                ))}
+              )}
+              
               </div>
-            </div>
-            ))}
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+              </div>
+              </section>
+            );
 };
 
 export default NewItems;
